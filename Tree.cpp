c76ae3c8055527468d8ac324ca1a6545d384f26d
Bibek Dhungana <dhunganapiyush@gmail.com>
2024-02-12 22:07:46 +0545

    addNode, askQuestion, displayResult features added to tree

--- Tree.cpp
+++ Tree.cpp
@@ -1,65 +1,56 @@
-#include <iostream>
-#include <cstring>
+#include "Tree.h"
 
-using namespace std;
+Tree:: addNode(simple_string s, bool tf, Node* curr) {
+    Node *newNode = new Node(std::move(s));
+    if (root == nullptr) {
+        root = newNode;
+        return;
+    }
 
-class Tree {
-    private:
-        struct Node {
-            string question;
-            Node* lChild;
-            Node* rChild;
+    if (tf) curr->right = newNode;
+    else curr->left = newNode;
 
-            Node() {}
+}
 
-            Node(string s) : question(s), lChild(nullptr), rChild(nullptr) {}
-        };
+//        void updateLeaves() {
+//            Node* curr = root;
+//            if (curr->left == nullptr && curr->right == nullptr) leaves.push_back(curr);
+//            curr = curr->left;
+//        }
 
-        Node* root;
-        // vector<Node*> leaves;
-
-    public:
-        Tree() {
-            root->question = string("Does it breathe??");
-            string ansl = "Non-living", ansr = "Living";
-
-            // Node* lNode;
-            // lNode->question = ansl;
-            // root->lChild = lNode;
-
-            // Node* rNode;
-            // rNode->question = ansr;
-            // root->rChild = rNode;
+Tree:: Node* displayResult (Node* aNode) {
+    if (aNode->left == nullptr || aNode->right == nullptr) {
+        std::string answer;
+        std::cout << "Is your answer " << aNode->data << std::endl;
+        std::cin >> answer;
+        if (answer == "yes") {
+            std::cout << "Thanks for playing !!!";
+            return aNode;
         }
+        std::cout << "Please enter a question with a distinguishing trait for your answer.";
+        std::cin >> answer;
+        addNode(answer, false, aNode);
+        return aNode->right;
+    }
+    return nullptr;
+}
 
-        void askQuestion(Node* aNode) {
-            string answer;
-            cout << aNode->question << endl;
-            cin >> answer;
-            tolower(answer);
-            if (answer == "yes") cout << "aNode->rChild->question" << endl;
-            else cout << "aNode->lChild->question" << endl;
-        }
+//        void tolower(std::string st) {
+//            const char* s = st.c_str();
+//            for (int i = 0; i < strlen(s); i++) {
+//                if (st[i] >= 65 && st[i] <= 90) st[i] -= 32;
+//            }
+//        }
 
-        Node* nodeInit() {
-            
-        }
 
-        void display() {
-            askQuestion(root);
-        }
+Tree:: askQuestion(Tree::Node *qNode) {
 
-        void tolower(string st) {
-            const char* s = st.c_str();
-            for (int i = 0; i < strlen(s); i++) {
-                if (st[i] >= 65 && st[i] <= 90) st[i] -= 32;
-            }
-        }
+    simple_string answer;
+    std::cout << qNode->data;
+    std::cin >> answer;
 
-};
+    if (qNode->left == nullptr || qNode->right == nullptr) return displayResult(qNode);
 
-int main() {
-    Tree t;
-
-    t.display();
+    if (strcmp(answer, "yes")) askQuestion (qNode->right);
+    else askQuestion (qNode->left);
 }
